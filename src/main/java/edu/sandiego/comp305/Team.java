34801e88cd45f598c6cd39ac61785ad30d6b1664
Armaan Rai <armaanrai@sandiego.edu>
2026-04-28 11:22:15 -0700

    Feat: Method Stub for team class

---
+++ src/main/java/edu/sandiego/comp305/Team.java
@@ -0,0 +1,32 @@
+package edu.sandiego.comp305;
+
+public class Team {
+    private int ranking;
+    private String countryName;
+    private String countryCode;
+    private int rating;
+    private int totalGames;
+    private int totalWins;
+    private int totalLosses;
+    private int goalScored;
+    private int goalsConceded;
+
+    public Integer getRating(){
+        return 0;
+    }
+    public Double getWinRate(){
+        return 0.0;
+    }
+    public Integer getGoalDifference(){
+        return 0;
+    }
+    public Integer getRanking(){
+        return 0;
+    }
+    public String getCountryName(){
+        return "country";
+    }
+    public String getCountryCode(){
+        return "code";
+    }
+}
