abd99c2fd86b61f114dca4c6c9f9f92493f31da8
Armaan Rai <armaanrai@sandiego.edu>
2026-04-28 15:20:55 -0700

    Feat: Added method stubs for Group

---
+++ src/main/java/edu/sandiego/comp305/Group.java
@@ -0,0 +1,34 @@
+package edu.sandiego.comp305;
+
+import java.util.List;
+
+public class Group {
+    private final String name;
+
+    private final List<Team> teams;
+
+    private final List<Standings> standings;
+
+    public Group(final String name, final List<Team> teams,
+                 final List<Standings> standings){
+        this.name = name;
+        this.teams = teams;
+        this.standings = standings;
+    }
+
+    public void playGroupStage(){
+
+    }
+
+    public List<Team> getQualifiers(){
+        return null;
+    }
+
+    public List<Standings> getStandings(){
+        return null;
+    }
+
+    public String getName(){
+        return name;
+    }
+}
