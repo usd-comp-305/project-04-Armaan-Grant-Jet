ea1aefbb4a9e7a1788fa1a57ac3a9718404287df
Jet Tang Ro Starkweather <jstarkweather@sandiego.edu>
2026-04-28 15:39:39 -0700

    feature: method stub for csv loader that will load the teams from scraped data

---
+++ src/main/java/edu/sandiego/comp305/CSVLoader.java
@@ -0,0 +1,17 @@
+package edu.sandiego.comp305;
+
+import java.util.List;
+
+public class CSVLoader {
+
+    private final String filePath;
+
+    public CSVLoader(final String filePath) {
+        this.filePath = filePath;
+    }
+
+    public List<Team> loadTeams() {
+        return null;
+    }
+}
+
