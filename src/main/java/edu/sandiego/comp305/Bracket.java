e3ed0ea1bbaf58bfed80b7ddb01c867130c8e718
Armaan Rai <armaanrai@sandiego.edu>
2026-04-28 15:41:13 -0700

    Feat: Added method stubs to Bracket

---
+++ src/main/java/edu/sandiego/comp305/Bracket.java
@@ -0,0 +1,30 @@
+package edu.sandiego.comp305;
+
+import java.util.List;
+
+public class Bracket {
+    private final List<Team> teams;
+
+    private final int round;
+
+    private final PredictionStrategy strategy;
+
+    public Bracket(final List<Team> teams, final int round,
+                   final PredictionStrategy strategy){
+        this.teams = teams;
+        this.round = round;
+        this.strategy = strategy;
+    }
+
+    public List<Team> playRound(){
+        return null;
+    }
+
+    public Team getWinner(){
+        return null;
+    }
+
+    public Integer getCurrentRound(){
+        return round;
+    }
+}
