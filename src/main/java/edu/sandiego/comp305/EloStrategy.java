61d58b489fd630ac341236ba758b1565d49cd79e
Jet Tang Ro Starkweather <jstarkweather@sandiego.edu>
2026-04-28 15:03:15 -0700

    feature: method stub for elo strategy that implements prediction strategy

---
+++ src/main/java/edu/sandiego/comp305/EloStrategy.java
@@ -0,0 +1,9 @@
+package edu.sandiego.comp305;
+
+public class EloStrategy implements PredictionStrategy {
+    
+    @Override
+    public double getProbability(Team homeTeam, Team awayTeam) {
+        return 0.0;
+    }
+}
