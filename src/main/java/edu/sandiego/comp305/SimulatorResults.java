3c20e3c37831f6d5750635f1875f4d89b4720558
Jet Tang Ro Starkweather <jstarkweather@sandiego.edu>
2026-04-28 16:21:23 -0700

    fix: checkstyle issues

--- src/main/java/edu/sandiego/comp305/SimulatorResults.java
+++ src/main/java/edu/sandiego/comp305/SimulatorResults.java
@@ -1,5 +1,6 @@
 package edu.sandiego.comp305;
 
+import java.util.HashMap;
 import java.util.Map;
 
 public class SimulatorResults {
@@ -10,13 +11,13 @@ public class SimulatorResults {
 
     public SimulatorResults(final Map<Team, Integer> winCounts, 
         final int totalRuns) {
-        this.winCounts = winCounts;
+        this.winCounts = new HashMap<>(winCounts);
         this.totalRuns = totalRuns;
     }
 
-    public void recordWin(Team team) {}
+    public void recordWin(final Team team) {}
 
-    public double getWinProbability(Team team) {
+    public double getWinProbability(final Team team) {
         return 0.0;
     }
 
