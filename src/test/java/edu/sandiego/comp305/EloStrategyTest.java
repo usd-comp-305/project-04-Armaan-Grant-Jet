53bf6a44196a35dbc05c424f02957641cb993eaf
Jet Tang Ro Starkweather <jstarkweather@sandiego.edu>
2026-04-30 16:17:26 -0700

    test: added rest of the test cases to check that the probability is correctly calculated

--- src/test/java/edu/sandiego/comp305/EloStrategyTest.java
+++ src/test/java/edu/sandiego/comp305/EloStrategyTest.java
@@ -57,5 +57,56 @@ public class EloStrategyTest {
 
         assertTrue(probability >= 0.0 && probability <= 1.0);
         assertFalse(Double.isNaN(probability));
+        assertTrue(probability > 0.5);
+    }
+
+    @Test
+    void probabilitieSumToOne() {
+        final double probabilityA =  strategy.getProbability(strongTeam, weakTeam);
+        final double probabilityB =  strategy.getProbability(weakTeam, strongTeam);
+
+        assertEquals(1.0, probabilityA + probabilityB, 1e-9);
+    }
+
+    @Test
+    void getProbabilityIdenticalTeamsReturnHalf() {
+        assertEquals(0.5, strategy.getProbability(equalTeamA, equalTeamB), 1e-9);
+    }
+
+    @Test
+    void strongerTeamIsFavoredToWin() {
+        assertTrue(strategy.getProbability(strongTeam, weakTeam) > 0.5);
+    }
+
+    @Test
+    void higherRatingIncreasesProbability() {
+        final Team higherRatedTeam = new Team(1, "High", "H", 2000, 100, 50, 25, 130, 100);
+        final Team lowerRatedTeam = new Team(2, "Low", "L", 1600, 100, 50, 25, 130, 100);
+
+        assertTrue(strategy.getProbability(higherRatedTeam, lowerRatedTeam) > 0.5);
+    }
+
+    @Test
+    void higherWinRateIncreasesProbability() {
+        final Team moreWinsTeam = new Team(1, "moreWins", "MW", 1800, 100, 70, 15, 130, 100);
+        final Team fewerWinsTeam = new Team(2, "fewerWins", "FW", 1800, 100, 30, 35, 130, 100);
+
+        assertTrue(strategy.getProbability(moreWinsTeam, fewerWinsTeam) > 0.5);
+    }
+
+    @Test
+    void betterDefenseIncreasesProbability() {
+        final Team goodDefense =  new Team(1, "goodDefense", "GD", 1800, 100, 50, 25, 130, 60);
+        final Team badDefense = new Team(2, "badDefense", "BD", 1800, 100, 50, 25, 130, 180);
+
+        assertTrue(strategy.getProbability(goodDefense, badDefense) > 0.5);
+    }
+
+    @Test
+    void betterGoalDifferenceIncreasesProbability() {
+        final Team betterGoalDiff = new Team(1, "betterGoalDifference", "BD", 1800, 100, 50, 25, 180, 80);
+        final Team worseGoalDiff = new Team(2, "worseGoalDifference", "WD", 1800, 100, 50, 25, 80, 180);
+
+        assertTrue(strategy.getProbability(betterGoalDiff, worseGoalDiff) > 0.5);
     }
 }
