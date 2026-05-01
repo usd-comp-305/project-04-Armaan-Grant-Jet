b813a13bbf8d9909606ec5c74f2914716dae2235
GrantHigdon <ghigdon@sandiego.edu>
2026-04-30 18:35:07 -0700

    test: verify Team stores and returns country code

--- src/test/java/edu/sandiego/comp305/TeamTest.java
+++ src/test/java/edu/sandiego/comp305/TeamTest.java
@@ -9,4 +9,10 @@ public class TeamTest {
         Team team = new Team(1, "Brazil", "BRA", 2141, 100, 70, 10, 200, 80);
         assertEquals("Brazil", team.getCountryName());
     }
+
+    @Test
+    void teamHasCorrectCountryCode() {
+        Team team = new Team(3, "France", "FRA", 2050, 90, 60, 15, 180, 70);
+        assertEquals("FRA", team.getCountryCode());
+    }
 }
