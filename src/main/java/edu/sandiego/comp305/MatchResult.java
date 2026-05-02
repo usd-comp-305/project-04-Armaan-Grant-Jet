63895a0dceb2ef7bf3f4e00f1ba92d03190ff1d0
Jet Tang Ro Starkweather <jstarkweather@sandiego.edu>
2026-05-02 13:56:08 -0700

    refactor: changed variable names to something that makes more sense

--- src/main/java/edu/sandiego/comp305/MatchResult.java
+++ src/main/java/edu/sandiego/comp305/MatchResult.java
@@ -3,17 +3,17 @@ package edu.sandiego.comp305;
 public class MatchResult {
     private final Team winner;
 
-    private final int goalsA;
+    private final int homeTeamGoals;
 
-    private final int  goalsB;
+    private final int awayTeamGoals;
 
     private final boolean isDraw;
 
     public MatchResult(final Team winner, final int goalsA,
                        final int goalsB, final boolean isDraw){
         this.winner = winner;
-        this.goalsA = goalsA;
-        this.goalsB = goalsB;
+        this.homeTeamGoals = goalsA;
+        this.awayTeamGoals = goalsB;
         this.isDraw = isDraw;
     }
 
@@ -25,11 +25,11 @@ public class MatchResult {
         return isDraw;
     }
 
-    public Integer getGoalsA(){
-        return goalsA;
+    public Integer getHomeTeamGoals(){
+        return homeTeamGoals;
     }
 
-    public Integer getGoalsB(){
-        return goalsB;
+    public Integer getAwayTeamGoals(){
+        return awayTeamGoals;
     }
 }
