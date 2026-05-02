0828c31e993ddc27446967a39997e60a79cd8246
Jet Tang Ro Starkweather <jstarkweather@sandiego.edu>
2026-05-02 15:14:28 -0700

    feature: added a production and a testable constructor for mockito to keep testing and code easy and readable

--- src/main/java/edu/sandiego/comp305/Match.java
+++ src/main/java/edu/sandiego/comp305/Match.java
@@ -1,6 +1,14 @@
 package edu.sandiego.comp305;
 
+import java.util.Random;
+
 public class Match {
+
+    private static final double DRAW_SHARE = 0.35;
+
+    private static final double BASE_GOALS = 1.3;
+
+    private static final double PENALTY_WIN_PROBABILITY = 0.5;
 
     private final Team homeTeam;
 
@@ -8,14 +16,23 @@ public class Match {
 
     private final PredictionStrategy strategy;
 
-    private final boolean isKnockout; 
+    private final boolean isKnockout;
+
+    private final RandomProvider random;
 
     public Match(final Team homeTeam, final Team awayTeam, 
         final PredictionStrategy strategy, final boolean isKnockout) {
+        this(homeTeam, awayTeam, strategy, isKnockout, new Random()::nextDouble);
+    }
+    
+    public Match(final Team homeTeam, final Team awayTeam, 
+        final PredictionStrategy strategy, final boolean isKnockout, 
+        final RandomProvider random) {
         this.homeTeam = homeTeam;
         this.awayTeam = awayTeam;
         this.strategy = strategy;
         this.isKnockout = isKnockout;
+        this.random = random;
     }
 
     public MatchResult play() {
