20ab7dfec862fd9dc172d024050a202123dae685
Jet Tang Ro Starkweather <jstarkweather@sandiego.edu>
2026-05-02 15:39:13 -0700

    feature: added a method to run the a realistic match simulation

--- src/main/java/edu/sandiego/comp305/Match.java
+++ src/main/java/edu/sandiego/comp305/Match.java
@@ -36,11 +36,47 @@ public class Match {
     }
 
     public MatchResult play() {
-        return null;
+        final double homeWinProbability = strategy.getProbability(homeTeam, awayTeam);
+        final double drawProbability = (1.0 - homeWinProbability) * DRAW_SHARE;
+        
+        final double outcomeRoll = random.nextDouble();
+        if (outcomeRoll < homeWinProbability) {
+            final int homeGoals = simulateGoals(BASE_GOALS * 1.2);
+            final int awayGoals = simulateGoals(BASE_GOALS * 0.8);
+            final int guaranteedWinnerGoals = Math.max(homeGoals, awayGoals + 1);
+            
+            return new MatchResult(homeTeam, guaranteedWinnerGoals, awayGoals, false);
+        } else if (outcomeRoll < homeWinProbability + drawProbability) {
+            final int goals = simulateGoals(BASE_GOALS);
+            if (isKnockout) {
+                final Team winner = simulatePenalties();
+
+                return new MatchResult(winner, goals, goals, false);
+            }
+            return new MatchResult(null, goals, goals, true);
+        } else {
+            final int homeGoals = simulateGoals(BASE_GOALS * 0.8);
+            final int awayGoals = simulateGoals(BASE_GOALS * 1.2);
+            final int guaranteedWinnerGoals = Math.max(awayGoals, homeGoals + 1);
+
+            return new MatchResult(awayTeam, homeGoals, guaranteedWinnerGoals, false);
+        }
     }
 
     public Team simulatePenalties() {
         return null;
+    }
+
+    private int simulateGoals(final double lambda) {
+        final double threshold = Math.exp(-lambda);
+        double product = 1.0;
+        int goals = 0;
+        do {
+            product *= random.nextDouble();
+            goals++;
+        } while (product > threshold);
+        
+        return goals - 1;
     }
 }
 
