67bca5f0f5d2fc7dc19824d193fca8477357ea8f
Armaan Rai <armaanrai@sandiego.edu>
2026-05-06 18:02:31 -0700

    Test: Added failed test for getTotalRuns()

--- src/test/java/edu/sandiego/comp305/SimulatorResultsTest.java
+++ src/test/java/edu/sandiego/comp305/SimulatorResultsTest.java
@@ -23,4 +23,19 @@ public class SimulatorResultsTest {
         assertEquals(1, results.getWinProbability(spain), 0.01);
     }
 
+    @Test
+    public void testGetTotalRuns() {
+        final SimulatorResults results = new SimulatorResults();
+        final Team spain = new Team(1, "Spain", "ES", 2165,
+                780, 461, 138, 1591, 697);
+
+        final Team argentina = new Team(2, "Argentina", "AR", 2113,
+                1109, 610, 228, 2112, 1136);
+
+        results.recordWin(spain);
+        results.recordWin(argentina);
+
+        assertEquals(2, results.getTotalRuns());
+    }
+
 }
