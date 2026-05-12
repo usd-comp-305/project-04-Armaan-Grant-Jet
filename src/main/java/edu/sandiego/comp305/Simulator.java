3c8356e4b645727776a45fe9b0673d659066329e
Armaan Rai <armaanrai@sandiego.edu>
2026-05-12 09:09:56 -0700

    Removed injected loader

--- src/main/java/edu/sandiego/comp305/Simulator.java
+++ src/main/java/edu/sandiego/comp305/Simulator.java
@@ -6,13 +6,10 @@ public class Simulator {
 
     private final SimulatorResults result;
 
-    private final CSVLoader loader;
-
     public Simulator(final Tournament tournament, 
         final SimulatorResults result, final CSVLoader loader) {
         this.tournament = tournament;
         this.result = result;
-        this.loader = loader;
     }
 
     public void runSimulations(final int numSimRun) {
