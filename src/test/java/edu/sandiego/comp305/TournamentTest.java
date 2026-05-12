6f42acdbdafeb71cf8fd9298c7303ec2f094a35c
Armaan Rai <armaanrai@sandiego.edu>
2026-05-11 17:49:57 -0700

    Test: Added failed test case for buildBracket()

--- src/test/java/edu/sandiego/comp305/TournamentTest.java
+++ src/test/java/edu/sandiego/comp305/TournamentTest.java
@@ -38,4 +38,36 @@ public class TournamentTest {
         assertNotNull(groupA.getStandings());
 
     }
+    @Test
+    public void testBuildBracket() {
+        final List<Team> teams = new ArrayList<>();
+
+        final Team spain = new Team(1, "Spain", "Es", 2165,
+                780, 461, 138, 1591, 697);
+
+        final Team argentina = new Team(2, "Argentina", "AR", 2113,
+                1109, 610, 228, 2112, 1136);
+
+        final Team france = new Team(3, "France", "FR", 2082,
+                938, 474, 269, 1706, 1272);
+
+        final Team england = new Team(4, "England", "EN", 2020,
+                1161, 683, 216, 2719, 1120);
+
+        teams.add(spain);
+        teams.add(argentina);
+        teams.add(france);
+        teams.add(england);
+
+        final Group groupA = new Group("A", teams);
+        final List<Group> groups = new ArrayList<>();
+        groups.add(groupA);
+
+        final Tournament tournament = new Tournament(groups);
+        tournament.runGroupStage();
+        tournament.buildBracket();
+        final Team winner = tournament.runKnockout();
+        assertNotNull(winner);
+    }
+
 }
