718c006d16ac86881365865d91260990dd9bed69
Armaan Rai <armaanrai@sandiego.edu>
2026-05-12 17:18:05 -0700

    Added the BeforeEach for test cases

--- src/test/java/edu/sandiego/comp305/TournamentFactoryTest.java
+++ src/test/java/edu/sandiego/comp305/TournamentFactoryTest.java
@@ -1,5 +1,23 @@
 package edu.sandiego.comp305;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+import java.util.List;
+
+
 public class TournamentFactoryTest {
+
+    private List<Team> teams;
+
+    @BeforeEach
+    public void setUp() {
+        final CSVLoader loader = new CSVLoader(
+                "data/test_teams.csv");
+
+        teams = loader.loadTeams();
+    }
+
+
 
 }
