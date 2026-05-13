60e9ee091784e5728ecdd9dc92e02e17b57e7c3d
Armaan Rai <armaanrai@sandiego.edu>
2026-05-12 17:31:33 -0700

    Implemented buildGroups

--- src/main/java/edu/sandiego/comp305/TournamentFactory.java
+++ src/main/java/edu/sandiego/comp305/TournamentFactory.java
@@ -12,7 +12,23 @@ public class TournamentFactory {
     }
 
     public static List<Group> buildGroups(final List<Team> teams) {
-        return null;
+        final Map<String, Team> teamMap = buildTeamMap(teams);
+
+        final List<Group> groups = new ArrayList<>();
+        groups.add(buildGroup("A", teamMap, List.of("MX", "KR", "ZA", "CZ")));
+        groups.add(buildGroup("B", teamMap, List.of("CA", "CH", "QA", "BA")));
+        groups.add(buildGroup("C", teamMap, List.of("BR", "MA", "SQ", "HT")));
+        groups.add(buildGroup("D", teamMap, List.of("US", "PY", "AU", "TR")));
+        groups.add(buildGroup("E", teamMap, List.of("DE", "EC", "CI", "CW")));
+        groups.add(buildGroup("F", teamMap, List.of("NL", "JP", "TN", "Se")));
+        groups.add(buildGroup("G", teamMap, List.of("BE", "IR", "EG", "NZ")));
+        groups.add(buildGroup("H", teamMap, List.of("ES", "UY", "SA", "CV")));
+        groups.add(buildGroup("I", teamMap, List.of("FR", "SN", "NO", "IQ")));
+        groups.add(buildGroup("J", teamMap, List.of("AR", "AT", "DZ", "JO")));
+        groups.add(buildGroup("K", teamMap, List.of("PT", "CO", "UZ", "CG")));
+        groups.add(buildGroup("L", teamMap, List.of("EN", "HR", "PA", "GH")));
+
+        return groups;
     }
 
     private static Map<String, Team> buildTeamMap(final List<Team> teams) {
