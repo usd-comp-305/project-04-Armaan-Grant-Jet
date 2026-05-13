a318626933c7f7c56394ab5453d69034260a0f10
Armaan Rai <armaanrai@sandiego.edu>
2026-05-12 17:14:53 -0700

    Added method stubs for TournamentFactory

---
+++ src/main/java/edu/sandiego/comp305/TournamentFactory.java
@@ -0,0 +1,28 @@
+package edu.sandiego.comp305;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class TournamentFactory {
+
+    private TournamentFactory() {
+        //prevent instantiation
+    }
+
+    public static List<Group> buildGroups(final List<Team> teams) {
+        return null;
+    }
+
+    private static Map<String, Team> buildTeamMap(final List<Team> teams) {
+        return null;
+    }
+
+    private static Group buildGroup(final String name,
+                                    final Map<String, Team> teamMap,
+                                    final List<String> codes) {
+        return null;
+    }
+
+}
